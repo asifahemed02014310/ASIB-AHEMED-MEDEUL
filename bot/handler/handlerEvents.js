eb70377453ab75c6304ddbc1ac862ff76a2ac03b
Nur Hamim Badhon <113907821+nurhamimbadhon@users.noreply.github.com>
2025-05-13 21:18:24 +0600

    Update handlerEvents.js

--- bot/handler/handlerEvents.js
+++ bot/handler/handlerEvents.js
@@ -307,33 +307,119 @@ module.exports = function (api, threadModel, userModel, dashBoardModel, globalMo
 			const time = getTime("DD/MM/YYYY HH:mm:ss");
 			isUserCallCommand = true;
 			try {
-				// analytics command call
-				(async () => {
-					const analytics = await globalData.get("analytics", "data", {});
-					if (!analytics[commandName])
-						analytics[commandName] = 0;
-					analytics[commandName]++;
-					await globalData.set("analytics", analytics, "data");
-				})();
+async function onStart() {
+	// —————————————— CHECK USE BOT —————————————— //
+	// Fallback to messageReply or message text if body is missing
+	let adjustedBody = body || event.messageReply?.body || event.message?.text || '';
+	if (!adjustedBody.startsWith(prefix))
+		return;
+	const dateNow = Date.now();
+	const args = adjustedBody.slice(prefix.length).trim().split(/ +/);
+	// ————————————  CHECK HAS COMMAND ——————————— //
+	let commandName = args.shift().toLowerCase();
+	let command = GoatBot.commands.get(commandName) || GoatBot.commands.get(GoatBot.aliases.get(commandName));
+	// ———————— CHECK ALIASES SET BY GROUP ———————— //
+	const aliasesData = threadData.data.aliases || {};
+	for (const cmdName in aliasesData) {
+		if (aliasesData[cmdName].includes(commandName)) {
+			command = GoatBot.commands.get(cmdName);
+			break;
+		}
+	}
+	// ————————————— SET COMMAND NAME ————————————— //
+	if (command)
+		commandName = command.config.name;
+	// ——————— FUNCTION REMOVE COMMAND NAME ———————— //
+	function removeCommandNameFromBody(body_, prefix_, commandName_) {
+		if (arguments.length) {
+			if (typeof body_ != "string")
+				throw new Error(`The first argument (body) must be a string, but got "${getType(body_)}"`);
+			if (typeof prefix_ != "string")
+				throw new Error(`The second argument (prefix) must be a string, but got "${getType(prefix_)}"`);
+			if (typeof commandName_ != "string")
+				throw new Error(`The third argument (commandName) must be a string, but got "${getType(commandName_)}"`);
 
-				createMessageSyntaxError(commandName);
-				const getText2 = createGetText2(langCode, `${process.cwd()}/languages/cmds/${langCode}.js`, prefix, command);
-				await command.onStart({
-					...parameters,
-					args,
-					commandName,
-					getLang: getText2,
-					removeCommandNameFromBody
-				});
-				timestamps[senderID] = dateNow;
-				log.info("CALL COMMAND", `${commandName} | ${userData.name} | ${senderID} | ${threadID} | ${args.join(" ")}`);
+			return body_.replace(new RegExp(`^${prefix_}(\\s+|)${commandName_}`, "i"), "").trim();
+		}
+		else {
+			return adjustedBody.replace(new RegExp(`^${prefix}(\\s+|)${commandName}`, "i"), "").trim();
+		}
+	}
+	// —————  CHECK BANNED OR ONLY ADMIN BOX  ————— //
+	if (isBannedOrOnlyAdmin(userData, threadData, senderID, threadID, isGroup, commandName, message, langCode))
+		return;
+	if (!command)
+		if (!hideNotiMessage.commandNotFound))
+			return await message.reply(
+				commandName ?
+					utils.getText({ lang: langCode, head: "handlerEvents" }, "commandNotFound", commandName, prefix) :
+					utils.getText({ lang: langCode, head: "handlerEvents" }, "commandNotFound2", prefix)
+			);
+		else
+			return true;
+	// ————————————— CHECK PERMISSION ———————————— //
+	const roleConfig = getRoleConfig(utils, command, isGroup, threadData, commandName);
+	const needRole = roleConfig.onStart;
+
+	if (needRole > role) {
+		if (!hideNotiMessage.needRoleToUseCmd) {
+			if (needRole === 1) {
+				return await message.reply(utils.getText({ lang: langCode, head: "handlerEvents" }, "onlyAdmin", commandName));
 			}
-			catch (err) {
-				log.err("CALL COMMAND", `An error occurred when calling the command ${commandName}`, err);
-				return await message.reply(utils.getText({ lang: langCode, head: "handlerEvents" }, "errorOccurred", time, commandName, removeHomeDir(err.stack ? err.stack.split("\n").slice(0, 5).join("\n") : JSON.stringify(err, null, 2))));
+			else if (needRole === 2) {
+				return await message.reply(utils.getText({ lang: langCode, head: "handlerEvents" }, "onlyAdminBot2", commandName));
+			}
+			else if (needRole === 3) {
+				return await message.reply("❌ | Only the bot owner can use this command.");
 			}
 		}
+		else {
+			return true;
+		}
+	}
+	// ———————————————— countDown ———————————————— //
+	if (!client.countDown[commandName])
+		client.countDown[commandName] = {};
+	const timestamps = client.countDown[commandName];
+	let getCoolDown = command.config.countDown;
+	if (!getCoolDown && getCoolDown != 0 || isNaN(getCoolDown))
+		getCoolDown = 1;
+	const cooldownCommand = getCoolDown * 1000;
+	if (timestamps[senderID]) {
+		const expirationTime = timestamps[senderID] + cooldownCommand;
+		if (dateNow < expirationTime)
+			return await message.reply(utils.getText({ lang: langCode, head: "handlerEvents" }, "waitingForCommand", ((expirationTime - dateNow) / 1000).toString().slice(0, 3)));
+	}
+	// ——————————————— RUN COMMAND ——————————————— //
+	const time = getTime("DD/MM/YYYY HH:mm:ss");
+	isUserCallCommand = true;
+	try {
+		// analytics command call
+		(async () => {
+			const analytics = await globalData.get("analytics", "data", {});
+			if (!analytics[commandName])
+				analytics[commandName] = 0;
+			analytics[commandName]++;
+			await globalData.set("analytics", analytics, "data");
+		})();
 
+		createMessageSyntaxError(commandName);
+		const getText2 = createGetText2(langCode, `${process.cwd()}/languages/cmds/${langCode}.js`, prefix, command);
+		await command.onStart({
+			...parameters,
+			args,
+			commandName,
+			getLang: getText2,
+			removeCommandNameFromBody
+		});
+		timestamps[senderID] = dateNow;
+		log.info("CALL COMMAND", `${commandName} | ${userData.name} | ${senderID} | ${threadID} | ${args.join(" ")}`);
+	}
+	catch (err) {
+		log.err("CALL COMMAND", `An error occurred when calling the command ${commandName}`, err);
+		return await message.reply(utils.getText({ lang: langCode, head: "handlerEvents" }, "errorOccurred", time, commandName, removeHomeDir(err.stack ? err.stack.split("\n").slice(0, 5).join("\n") : JSON.stringify(err, null, 2))));
+	}
+}
 
 		/*
 		 +------------------------------------------------+
